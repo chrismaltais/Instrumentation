a8fe879c488f61e13032504038b52f09e2f0f6c7
Chris Maltais <chris.maltais@hotmail.ca>
2019-03-06 14:09:41 -0500

    ✅ Add test file

--- src/TestFile.java
+++ src/TestFile.java
@@ -9,31 +9,64 @@ class TestFile {
 		// Get numbers between 1 - 99999
 		for (int i = 0; i < size; i ++) {
 			int randomNumber = rand.nextInt(99999) + 1;
-			System.out.println(randomNumber);
 			numList[i] = randomNumber;
 		}
 		return numList;
     }
     
     public static void main(String[] args) {
+    	// Define instrumentation object
+    	Instrumentation instrumentation = Instrumentation.getInstance();
+    	
+    	// Activate instrumentation object:
+    	instrumentation.activate(true);
+    	
+    	// Begin program test
+    	instrumentation.startTiming("Main");
+    	
+    	// Instantiate all sorting algorithms
     	BubbleSort2Algorithm bubbleSort = new BubbleSort2Algorithm();
     	ExtraStorageMergeSortAlgorithm mergeSort = new ExtraStorageMergeSortAlgorithm();
     	NaiveQuickSortAlgorithm quickSort = new NaiveQuickSortAlgorithm();
     	SelectionSortAlgorithm selectionSort = new SelectionSortAlgorithm();
     	
-    	int arraySize = 25;
-    	int[] testValues = populateArray(arraySize);
-    	int[] copyTestValues = testValues.clone();
+    	// Create a test array
+    	int arraySize = 5;
+    	int[] initializeTestValues = populateArray(arraySize);
+    	int[] testValues = initializeTestValues.clone();
     	
-    	System.out.println(copyTestValues[0]);
-    	System.out.println("copy ^, original DOWN");
-    	System.out.println(testValues[0]);
-    	
+    	// Time the Bubble sort
+    	instrumentation.startTiming("Bubble Sort");
     	bubbleSort.sort(testValues);
-    	System.out.println("--------------------");
-    	for (int num : testValues) {
-    		System.out.println(num);
-    	}
+    	instrumentation.stopTiming("Bubble Sort");
+    	
+    	// Reinitialize array to be sorted
+    	testValues = initializeTestValues.clone();
+    	
+    	instrumentation.comment("This is a test comment");
+    	
+    	// Time the Merge Sort
+    	instrumentation.startTiming("Merge Sort");
+    	mergeSort.sort(testValues);
+    	instrumentation.stopTiming("Merge Sort");
+    	
+    	// Reinitialize array to be sorted
+    	testValues = initializeTestValues.clone();
+    	
+    	// Time the Merge Sort
+    	instrumentation.startTiming("Quick Sort");
+    	quickSort.sort(testValues);
+    	instrumentation.stopTiming("Quick Sort");
+    	
+      	// Reinitialize array to be sorted
+    	testValues = initializeTestValues.clone();
+    	
+    	// Time the Merge Sort
+    	instrumentation.startTiming("Selection Sort");
+    	selectionSort.sort(testValues);
+    	instrumentation.stopTiming("Selection Sort");
+    	
+    	instrumentation.stopTiming("Main");
     	
     }
 }
